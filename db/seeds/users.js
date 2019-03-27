b831537d2a9acc118b0618b191fe276addb8db4c
reshux <duruhanunsal@gmail.com>
2019-03-27 20:16:43 +0000

    Erronous seed file

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -1,12 +1,12 @@
 exports.seed = function(knex, Promise) {
   return Promise.all([
     knex.raw("ALTER SEQUENCE users_id_seq RESTART WITH 1"),
-    knex('users').then(function () {
+    knex("users").then(function() {
       return Promise.all([
-        knex('users').insert({name: 'Alice'}),
-        knex('users').insert({name: 'Bob'}),
-        knex('users').insert({name: 'Charlie'})
+        knex("users").insert({ name: "Alice" }),
+        knex("users").insert({ name: "Bob" }),
+        knex("users").insert({ name: "Charlie" })
       ]);
     })
-  ])
+  ]);
 };

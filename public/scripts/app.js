0d57314354c02b6ca6040f94ffa18ff29048a0ab
reshux <duruhanunsal@gmail.com>
2019-03-27 21:24:17 +0000

    Some minor modifications

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,65 @@
+// const myArray = [
+//   { position: { lat: 45.558, lng: -73.5519 }, label: "A", title: "Olympic" },
+//   {
+//     position: { lat: 38.8718568, lng: -77.0562669 },
+//     label: "B",
+//     title: "Pentagon"
+//   }
+// ];
+
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: { lat: 45.5017, lng: -73.5673 }
+  });
+  // const localCopy = myArray;
+  // for (var i = 0; i < localCopy.length; i++) {
+  //   var marker = new google.maps.Marker({
+  //     position: localCopy[i].position,
+  //     map: map,
+  //     label: localCopy[i].label
+  //   });
+  // }
+}
+
+// function createListRow(listItem) {
+//   const $tableRow = $("<tr>").append(
+//     $("<td>").text(listItem.label),
+//     $("<td>").text(listItem.title)
+//   );
+//   return $tableRow;
+// }
+
+// function renderList(data) {
+//   $("#testTable").empty();
+//   data.forEach(item => {
+//     let rendered = createListRow(item);
+//     $("#testTable").append(rendered);
+//   });
+// }
+
 $(() => {
+  // renderList(myArray);
+  // $.ajax({
+  //   method: "GET",
+  //   url: "/api/users"
+  // }).done(users => {
+  //   console.log(users);
+  //   for (user of users) {
+  //     $("<div>")
+  //       .text(user.name)
+  //       .appendTo($("#listDisplay"));
+  //   }
+  // });
   $.ajax({
     method: "GET",
     url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+  }).done(lists => {
+    console.log(lists);
+    for (row of lists) {
+      $("<div>")
+        .text(row.title)
+        .appendTo($("#listDisplay"));
     }
-  });;
+  });
 });
--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,17 @@
 "use strict";
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
-module.exports = (knex) => {
-
+module.exports = knex => {
   router.get("/", (req, res) => {
     knex
       .select("*")
       .from("users")
-      .then((results) => {
+      .then(results => {
         res.json(results);
-    });
+      });
   });
 
   return router;
-}
+};

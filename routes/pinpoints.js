1e3c3438757f0a6e577cee94cc927474b5990521
reshux <duruhanunsal@gmail.com>
2019-03-28 14:26:10 +0000

    Route modified to work with list_id

--- routes/pinpoints.js
+++ routes/pinpoints.js
@@ -2,12 +2,14 @@
 
 const express = require("express");
 const router = express.Router();
+const list_id = 2;
 
 module.exports = knex => {
   router.get("/", (req, res) => {
     knex
       .select("*")
       .from("pinpoints")
+      .where({ list_id })
       .then(results => {
         res.json(results);
       });

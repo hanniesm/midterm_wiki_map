85fbb7bc1ca3566493a7c45650362cd5506633b4
reshux <duruhanunsal@gmail.com>
2019-03-28 18:08:39 +0000

    JQuery library problem fixed

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -31,6 +31,7 @@ function initMap() {
     url: "/api/pinpoints"
   }).done(pinpoints => {
     for (var i = 0; i < pinpoints.length; i++) {
+      // com;
       // if (pinpoints[i].list_id === 1) {
       markerArray.push({
         label: labelString[placedMarkerCounter],

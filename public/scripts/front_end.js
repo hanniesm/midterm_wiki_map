81773a7de485bd57823bcf89ef579f333fc66a09
Hannah <hanniesutton@gmail.com>
2019-03-28 16:40:55 +0000

    add basics for creating lists

--- public/scripts/front_end.js
+++ public/scripts/front_end.js
@@ -17,3 +17,72 @@ $(document).ready(function(){
     console.log("edit map")
   });
 });
+
+
+function renderLists(lists) {
+  $("#accordion").empty();
+  for (const listObj of lists) {
+    const $list = createListElement(listObj);
+    $("#accordion").append($list);
+  }
+};
+
+
+function createListElement(list) {
+//create card
+  const $list = $("<div>").addClass("card");
+  const $cardHeader = $("<div>").addClass("card-header");
+  const $button = $("<div>").addClass("button");
+
+  $("<h2>").text(list.title).appendTo($button);
+  $button.appendTo($cardHeader);
+
+  const $icons = $("<div>").addClass("icons");
+
+  $("<i>").addClass("far fa-star").appendTo($icons);
+  $("<i>").addClass("far fa-edit").appendTo($icons);
+
+  $icons.appendTo($cardHeader);
+  $cardHeader.appendTo($list);
+
+  const $cardBody = $("<div>").addClass("card-body");
+
+  $("<p>").attr("id", "description").text(list.description).appendTo($cardBody)
+  $("<p>").attr("id", "created_by").text(list.created_by).appendTo($cardBody)
+  $("<p>").attr("id", "collaborators").text(list.collaborators).appendTo($cardBody)
+
+  $cardBody.appendTo($list)
+
+  return $list;
+};
+
+const request = (options, cb) => {
+  $.ajax(options)
+    .done(response => {
+      cb(response);
+    })
+
+    .fail(error => {
+      console.log(`Error: ${error}`);
+    })
+
+    .always(() => {
+      console.log("Request completed");
+    })
+};
+
+const loadLists = () => {
+  $(document).ready(function() {
+    const url = "/lists/";
+
+    const requestOptions = {
+      method: "GET",
+      url: url,
+      dataType: "json",
+    };
+
+    request(requestOptions, function(response) {
+    renderTweets(response);
+    });
+  });
+};
